62ecb41169410fbda08078e57688ac3b02d24cf9
Swechhita <swechhitarimal10@gmail.com>
2025-12-10 15:25:02 +0545

    updated product summar

--- src/ProductSummary.tsx
+++ src/ProductSummary.tsx
@@ -1,20 +1,28 @@
+import { Blocks } from 'lucide-react';
+import { DollarSign } from 'lucide-react';
+import { Layers } from 'lucide-react';
+import { TriangleAlert } from 'lucide-react'
 
 function ProductSummary() 
 {
     return (
         <>
         <div className="flex items-center justify-between gap-4 mx-4 my-4">
-            <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md">
+            <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md flex justify-between">
                 <p className="text-gray-800">Total Products</p>
+                <Blocks className="w-6 h-6"></Blocks>
             </div>
-        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md">
+        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md flex justify-between">
             <p className="text-gray-800">Total Value</p>
+            <DollarSign className="w-6 h-6"></DollarSign>
             </div>
-        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md">
+        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md flex justify-between">
            <p className ="text-gray-800">Categories</p>
+           <Layers className="w-6 h-6"></Layers>           
             </div>
-        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md">
+        <div className="border border-gray-200 rounded-lg p-4 w-1/4 shadow-md flex justify-between">
             <p className="text-gray-800">Low Stock Items</p>
+            <TriangleAlert className="w-6 h-6 text-yellow-800"></TriangleAlert>
             </div>
         </div>
         </>
